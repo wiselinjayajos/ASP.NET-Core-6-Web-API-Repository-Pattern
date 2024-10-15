9fca9d92aeabe76ae66cefdf24e3d000e6bd9da6
Wiselin Jaya Jos <wiselin.jayajos@gmail.com>
2024-10-15 23:25:06 +0100

    Removed the duplicate files

--- HRISWebAPI/Repositories/IEmployeeRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using HRISWebAPI.Models;
-namespace HRISWebAPI.Repositories
-{
-    public interface IEmployeeRepository
-    {
-        Task<IEnumerable<Employee>> GetAllAsync(int pageNumber, int pageSize);
-        Task<Employee> GetByIdAsync(int id);
-    }
-}
